1cca6dcbc82aa54641172ade5a9e65d5f3662b08
SARAN-28 <saranponnusamy2853@gmail.com>
2026-01-23 10:41:16 +0530

    Modified changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AdminDashboard from "./pages/adminDashboard";
 import AdminRoute from "./routes/AdminRoute";
-
 
 function App() {
   return (
@@ -12,9 +12,10 @@ function App() {
 
       <Routes>
         <Route path="/" element={<h1>Welcome to Event Management</h1>} />
-        <Route path="/admin" element={<AdminRoute><AdminDashboard/></AdminRoute>} />
+        <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
       </Routes>
 
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClickpauseOnHover />
     </Router>
   );
 }
